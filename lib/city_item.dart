10904e8a75e2a93a8008c7ccf4ebc75b1a317c2d
conghaonet <conghaonet@gmail.com>
2020-04-02 05:07:41 +0800

    收缩城市列表

--- lib/city_item.dart
+++ lib/city_item.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterlistdemo/entity.dart';
+import 'package:flutterlistdemo/province_notifier.dart';
+import 'package:provider/provider.dart';
 
-class CityItem extends StatelessWidget{
+class CityItem extends StatefulWidget{
   final int index;
   final CityEntity city;
 
   CityItem(this.index, this.city,);
 
   @override
+  _CityItemState createState() => _CityItemState();
+}
+
+class _CityItemState extends State<CityItem> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey.withOpacity(0.5),
-      child: Text('$index  ${city.code}  ${city.name}', style: TextStyle(fontSize: 16,),),
+    return InkWell(
+      onTap: () {
+        widget.city.hidden = !widget.city.hidden;
+        Provider.of<ProvinceNotifier>(context, listen: false).refreshProvince();
+      },
+      child: Container(
+        color: Colors.grey.withOpacity(0.5),
+        child: Text('${widget.index}  ${widget.city.code}  ${widget.city.name}', style: TextStyle(fontSize: 16,),),
+      ),
     );
   }
-
 }
